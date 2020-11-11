9bf08f1264964d2a5f6537b5e56bdddcbfd416e5
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-11 17:25:12 +0000

    Started writing assignment

--- src/test/java/assignment/Tests.java
+++ src/test/java/assignment/Tests.java
@@ -15,9 +15,10 @@ public class Tests {
 
 
    @Test
-   public void test(){
+   public void testTopFiveMovies(){
        App.main(null);
        //assertEquals(EXPECTED RESULT, App.METHODtoTEST, "ERROR MESSAGE");
+       assertEquals("a, b, c, d, e", App.topFiveMovies("a", "b", "c", "d", "e"), "INCORRECT: follow the formatting in the example");
    }
 
 }
